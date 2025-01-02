db129b8c235c607202fff3b55837333b63864cee
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-02 21:21:03 +0330

    add tray

---
+++ src/core/tray/index.ts
@@ -0,0 +1,21 @@
+import { TrayIcon, TrayIconEvent } from '@tauri-apps/api/tray';
+import { currentWindow } from '@/core/window';
+import trayMenu from '@/core/tray/menu';
+
+const tray = await TrayIcon.new({
+  id: 'break-reminder-tray',
+  menu: trayMenu,
+  tooltip: 'Break reminder',
+  menuOnLeftClick: false,
+  action: async (event: TrayIconEvent): Promise<void> => {
+    if (
+      event.type === 'Click' &&
+      event.button === 'Left' &&
+      event.buttonState === 'Up'
+    ) {
+      await currentWindow.show();
+    }
+  }
+});
+
+export default tray;
