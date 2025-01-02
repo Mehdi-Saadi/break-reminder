2c122c0c6c818b1806ff2dbd5919d34db617d5d9
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-02 22:11:33 +0330

    update icon

--- src/common/ui/icons/addIcon.ts
+++ src/common/ui/icons/addIcon.ts
@@ -1,3 +1,3 @@
-const addIcon = (classStyles?: string): string => `<svg ${classStyles} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>`;
+const addIcon = (classes: string = ''): string => `<svg class="${classes}" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>`;
 
 export default addIcon;
