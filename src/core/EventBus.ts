f2385b204f8cc19533ccb3baf2057c9588e64bbe
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-02 20:56:51 +0330

    add base event bus

---
+++ src/core/EventBus.ts
@@ -0,0 +1,19 @@
+type EventHandler = (payload: any) => void;
+
+export class EventBus {
+  private events: Record<string, EventHandler[]> = {};
+
+  on(event: string, handler: EventHandler): void {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+
+    this.events[event].push(handler);
+  }
+
+  emit(event: string, payload: any): void {
+    if (this.events[event]) {
+      this.events[event].forEach(handler => handler(payload));
+    }
+  }
+}
