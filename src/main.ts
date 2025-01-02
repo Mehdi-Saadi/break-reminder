57653bc3d993811ee15003369cbf919908157002
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-02 20:37:02 +0330

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
-window.addEventListener("DOMContentLoaded", () => {
-  alert('hello world!')
-});
+const app = document.getElementById('app')
+
+app!.innerHTML = '<span class="text-red-700 text-2xl">Hello World!</span>'
