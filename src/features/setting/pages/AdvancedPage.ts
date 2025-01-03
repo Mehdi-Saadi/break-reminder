6c028554d827e45ef93892b9b7b71d8521e9a282
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-03 17:44:13 +0330

    add base pages

--- src/features/setting/pages/AdvancedPage.ts
+++ src/features/setting/pages/AdvancedPage.ts
@@ -2,8 +2,8 @@ import Component from '@/common/ui/base/Component.ts';
 
 class SettingsPage extends Component {
   constructor() {
-    super('div', 'flex flex-col space-y-4');
-    this.element.innerText = 'Settings Page';
+    super('div', 'flex flex-col space-y-5 p-4 text-sm');
+    this.element.innerText = 'Advanced Page';
   }
 }
 
--- src/features/setting/pages/BreakMessagesPage.ts
+++ src/features/setting/pages/BreakMessagesPage.ts
@@ -3,7 +3,7 @@ import Component from '@/common/ui/base/Component.ts';
 class SettingsPage extends Component {
   constructor() {
     super('div', 'flex flex-col space-y-4');
-    this.element.innerText = 'Settings Page';
+    this.element.innerText = 'Break Messages Page';
   }
 }
 
