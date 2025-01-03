08aad233a1bc440acb8766ae2d62d08dc73f66b9
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-03 14:59:22 +0330

    add SidebarSettingButton

---
+++ src/common/ui/buttons/SidebarSettingButton.ts
@@ -0,0 +1,13 @@
+import SidebarNavigationButton from '@/common/ui/buttons/SidebarNavigationButton.ts'
+import icon from '@/common/ui/icons.ts'
+
+class SidebarSettingButton extends SidebarNavigationButton {
+  constructor() {
+    super(
+      icon('gear', 'size-5 me-1.5'),
+      'Settings'
+    )
+  }
+}
+
+export default SidebarSettingButton;
