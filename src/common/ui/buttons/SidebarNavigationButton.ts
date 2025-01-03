4f5d8df9386ba1c1dda5a7c6a5f5dc1e517fe9a4
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-03 14:58:42 +0330

    add base SidebarNavigationButton

---
+++ src/common/ui/buttons/SidebarNavigationButton.ts
@@ -0,0 +1,21 @@
+import Component from '@/common/ui/base/Component.ts'
+
+class SidebarNavigationButton extends Component {
+  constructor(htmlIcon: string, label: string) {
+    super('button', 'flex items-center pe-4 py-2 rounded-md text-sm');
+
+    this.element.setAttribute('type', 'button');
+
+    // button icon
+    this.element.innerHTML = htmlIcon;
+
+    // button label
+    const span = document.createElement('span');
+    span.setAttribute('class', 'pb-0.5');
+    span.innerText = label
+
+    this.element.appendChild(span);
+  }
+}
+
+export default SidebarNavigationButton;
