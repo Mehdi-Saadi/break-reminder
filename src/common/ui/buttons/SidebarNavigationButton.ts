0b6b97ecc0b369a06de6369afdb2d9f407ce8654
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-03 18:24:18 +0330

    update styles

--- src/common/ui/buttons/SidebarNavigationButton.ts
+++ src/common/ui/buttons/SidebarNavigationButton.ts
@@ -1,20 +1,34 @@
 import Component from '@/common/ui/base/Component.ts';
 
 abstract class SidebarNavigationButton extends Component {
+  private activeClasses: string[] = ['bg-[#eaeaea]', 'dark:bg-[#2d2d2d]', 'hover:bg-[#ededed]', 'hover:dark:bg-[#292929]'];
+  private notActiveClasses: string[] = ['hover:bg-[#eaeaea]', 'hover:dark:bg-[#2d2d2d]'];
+  protected activeIcon: HTMLDivElement;
+
   protected constructor(htmlIcon: string, label: string) {
-    super('button', 'flex items-center pe-4 py-2 rounded-md text-sm');
+    super('button', 'flex items-center pe-4 py-2 rounded-md text-sm w-full');
 
     this.element.setAttribute('type', 'button');
 
+    this.activeIcon = document.createElement('div');
+    this.activeIcon.setAttribute('class', 'min-w-0.5 min-h-4 max-w-0.5 max-h-4 bg-[#0067c0] rounded-full');
+
+    this.element.appendChild(this.activeIcon);
+
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'flex items-center ps-1');
+
     // button icon
-    this.element.innerHTML = htmlIcon;
+    wrapper.innerHTML = htmlIcon;
 
     // button label
     const span = document.createElement('span');
     span.setAttribute('class', 'pb-0.5');
     span.innerText = label
 
-    this.element.appendChild(span);
+    wrapper.appendChild(span);
+
+    this.element.appendChild(wrapper);
   }
 
   protected onMounted(): void {
@@ -26,6 +40,20 @@ abstract class SidebarNavigationButton extends Component {
   }
 
   protected abstract clickHandler(): void
+
+  protected setActive(): void {
+    this.element.classList.remove(...this.notActiveClasses);
+    this.element.classList.add(...this.activeClasses);
+
+    this.activeIcon.style.opacity = '1';
+  }
+
+  protected setNotActive(): void {
+    this.element.classList.remove(...this.activeClasses);
+    this.element.classList.add(...this.notActiveClasses);
+
+    this.activeIcon.style.opacity = '0';
+  }
 }
 
 export default SidebarNavigationButton;
