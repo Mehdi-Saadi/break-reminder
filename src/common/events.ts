4fdbe8764ddd751e55023a7595593b06bff2552d
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-03 19:57:50 +0330

    add NAVIGATION_PAGES

--- src/common/events.ts
+++ src/common/events.ts
@@ -3,5 +3,10 @@ import EventBus from '@/core/EventBus.ts'
 export const NAVIGATION_EVENTS = Object.freeze({
   NAVIGATE: 'navigate',
 });
-export type NavigationPage = 'settings' | 'break_messages' | 'advanced';
+export const NAVIGATION_PAGES = Object.freeze({
+  SETTINGS: 'settings',
+  BREAK_MESSAGES: 'break_messages',
+  ADVANCED: 'advanced',
+});
+export type NavigationPage = typeof NAVIGATION_PAGES[keyof typeof NAVIGATION_PAGES];
 export const pageNavEventBus = new EventBus();
