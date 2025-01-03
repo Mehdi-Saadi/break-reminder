770f439593d5b38e1fcd1f52df0d36a70ab97f70
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-03 15:06:42 +0330

    add SidebarBreakMessagesButton

---
+++ src/common/ui/buttons/SidebarBreakMessagesButton.ts
@@ -0,0 +1,13 @@
+import SidebarNavigationButton from '@/common/ui/buttons/SidebarNavigationButton.ts';
+import icon from '@/common/ui/icons.ts';
+
+class SidebarBreakMessagesButton extends SidebarNavigationButton {
+  constructor() {
+    super(
+      icon('chatBubble', 'size-5 me-1.5'),
+      'Break Messages'
+    )
+  }
+}
+
+export default SidebarBreakMessagesButton;
