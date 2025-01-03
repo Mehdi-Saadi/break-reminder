0906a33c866926f1404343322024ad81e1d64385
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-03 14:34:55 +0330

    add base sidebar class

---
+++ src/common/ui/layout/sidebar.ts
@@ -0,0 +1,9 @@
+import Component from '@/common/ui/base/component.ts'
+
+class Sidebar extends Component {
+  constructor() {
+    super('div', 'h-full w-1/3 fixed top-0 start-0 bg-inherit p-5 space-y-1')
+  }
+}
+
+export default Sidebar;
