8ce9adc2a632d857b97a2a86cdff17d9984dfb65
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-04 19:17:24 +0330

    add base NumberField

---
+++ src/features/setting/components/NumberField.ts
@@ -0,0 +1,78 @@
+import Component from '@/common/ui/base/Component.ts';
+import SquareAddButton from '@/common/ui/buttons/SquareAddButton.ts';
+import SquareRemoveButton from '@/common/ui/buttons/SquareRemoveButton.ts';
+
+class NumberField extends Component {
+  protected disableLayer: HTMLDivElement;
+  protected inputField: HTMLInputElement;
+  protected addButton: SquareAddButton;
+  protected removeButton: SquareRemoveButton;
+
+  constructor(
+    public step: number,
+    public max: number,
+  ) {
+    super('div', 'flex items-center border border-gray-300 rounded-md overflow-hidden relative');
+
+    this.disableLayer = document.createElement('div');
+    this.disableLayer.setAttribute('class', 'absolute size-full z-10 bg-transparent opacity-50');
+    this.element.appendChild(this.disableLayer);
+
+    this.inputField = document.createElement('input');
+    this.inputField.setAttribute('type', 'text');
+    this.inputField.setAttribute('class', 'h-6 w-9 focus:outline-0 px-1 bg-inherit border-e border-gray-300');
+    this.element.appendChild(this.inputField);
+
+    this.removeButton = new SquareRemoveButton(this.decrease);
+    this.removeButton.mount(this);
+
+    this.addButton = new SquareAddButton(this.increase);
+    this.addButton.mount(this);
+
+    this.enable();
+  }
+
+  enable(): void {
+    this.disableLayer.style.display = 'none';
+  }
+
+  disable(): void {
+    this.disableLayer.style.display = 'block';
+  }
+
+  private increase(): void {
+    const newValue = Number(this.inputField.value) + this.step;
+
+    if (newValue <= this.max) {
+      this.inputField.value = String(newValue);
+    }
+  }
+
+  private decrease(): void {
+    const newValue = Number(this.inputField.value) - this.step;
+
+    this.inputField.value = String(newValue < 0 ? 0 : newValue);
+  }
+
+  private onInput(): void {
+    this.inputField.value = this.inputField.value.replace(/\D/g, '');
+
+    if (!this.inputField.value) {
+      this.inputField.value = '0';
+    } else if (Number(this.inputField.value) > this.max) {
+      this.inputField.value = this.inputField.value.slice(0, this.inputField.value.length - 1);
+    }
+  }
+
+  protected onMounted(): void {
+    this.inputField.addEventListener('input', this.onInput);
+  }
+
+  protected onUnmounted(): void {
+    this.inputField.removeEventListener('input', this.onInput);
+    this.removeButton.unmount();
+    this.addButton.unmount();
+  }
+}
+
+export default NumberField;
