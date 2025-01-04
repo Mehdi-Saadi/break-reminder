fc8a4f36398abbed2a21a6589c21c0e39120040d
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-04 19:09:31 +0330

    implement click listener in onMounted and onUnmounted hooks

--- src/common/ui/buttons/SquareButton.ts
+++ src/common/ui/buttons/SquareButton.ts
@@ -1,10 +1,22 @@
 import Component from '@/common/ui/base/Component.ts'
 
 class SquareButton extends Component {
-  constructor() {
+  constructor(protected onClick?: (event: MouseEvent) => void) {
     super('button', 'flex items-center justify-center size-6 active:bg-[#eaeaea] hover:bg-[#ededed] active:dark:bg-[#2d2d2d] hover:dark:bg-[#292929]');
 
     this.element.setAttribute('type', 'button');
+  }
+
+  protected onMounted() {
+    if (this.onClick) {
+      this.element.addEventListener('click', this.onClick);
+    }
+  }
+
+  protected onUnmounted(): void {
+    if (this.onClick) {
+      this.element.removeEventListener('click', this.onClick);
+    }
   }
 }
 
