beb2a155db09b96f65a969f82bfb9b3781943ba3
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-04 18:43:53 +0330

    add base SquareAddButton

---
+++ src/common/ui/buttons/SquareAddButton.ts
@@ -0,0 +1,12 @@
+import SquareButton from '@/common/ui/buttons/SquareButton.ts'
+import icon from '@/common/ui/icons.ts'
+
+class SquareAddButton extends SquareButton {
+  constructor() {
+    super();
+
+    this.element.innerHTML = icon('add', 'size-4');
+  }
+}
+
+export default SquareAddButton;
