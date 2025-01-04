00395a5c241ca82b5a69be7f70929ad4c4f3ecff
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-04 19:10:43 +0330

    add onClick prop

--- src/common/ui/buttons/SquareAddButton.ts
+++ src/common/ui/buttons/SquareAddButton.ts
@@ -2,8 +2,8 @@ import SquareButton from '@/common/ui/buttons/SquareButton.ts';
 import icon from '@/common/ui/icons.ts';
 
 class SquareAddButton extends SquareButton {
-  constructor() {
-    super();
+  constructor(onClick?: (event: MouseEvent) => void) {
+    super(onClick);
 
     this.element.innerHTML = icon('add', 'size-4');
   }
--- src/common/ui/buttons/SquareRemoveButton.ts
+++ src/common/ui/buttons/SquareRemoveButton.ts
@@ -2,8 +2,8 @@ import SquareButton from '@/common/ui/buttons/SquareButton.ts';
 import icon from '@/common/ui/icons.ts';
 
 class SquareRemoveButton extends SquareButton {
-  constructor() {
-    super();
+  constructor(onClick?: (event: MouseEvent) => void) {
+    super(onClick);
 
     this.element.innerHTML = icon('remove', 'size-4');
   }
