07df220ce53070dd03d87fe5bba1dfcd0b9ae2c4
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-04 18:41:08 +0330

    add short break fields

--- src/features/setting/pages/SettingsPage.ts
+++ src/features/setting/pages/SettingsPage.ts
@@ -1,9 +1,23 @@
 import Component from '@/common/ui/base/Component.ts';
+import SettingItem from '@/features/setting/components/SettingItem.ts';
+import SettingSection from '@/features/setting/components/SettingSection.ts';
 
 class SettingsPage extends Component {
   constructor() {
     super('div', 'flex flex-col space-y-4');
-    this.element.innerText = 'Settings Page';
+
+    // short breaks
+    const shortBreaksSection = new SettingSection('Short Breaks');
+
+    const workDurationItem = new SettingItem('Work Duration (in minutes)');
+    workDurationItem.buttonContainer.innerText = 'some button';
+    workDurationItem.mount(shortBreaksSection.settingContainer);
+
+    const breakDurationItem = new SettingItem('Break Duration (in seconds)');
+    breakDurationItem.buttonContainer.innerText = 'some button';
+    breakDurationItem.mount(shortBreaksSection.settingContainer);
+
+    shortBreaksSection.mount(this);
   }
 }
 
