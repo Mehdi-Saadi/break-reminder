a61c3933c98737726eed40c902959170f18b920f
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-10 16:13:44 +0330

    add Milliseconds type

--- src/common/types.d.ts
+++ src/common/types.d.ts
@@ -1,4 +1,7 @@
+// time
 export type Minutes = number;
 export type Seconds = number;
+export type Milliseconds = number;
+
 export type UUID = `${string}-${string}-${string}-${string}-${string}`;
 export type BreakMessages = Record<UUID, string>;
