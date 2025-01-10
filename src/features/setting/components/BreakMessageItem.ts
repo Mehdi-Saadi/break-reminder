e67bde32573faba7ea78da76d5cec26ef636b30d
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-10 14:04:24 +0330

    add base BreakMessageItem

---
+++ src/features/setting/components/BreakMessageItem.ts
@@ -0,0 +1,18 @@
+import settingState from '@/core/state/SettingState.ts'
+import MessageInput from '@/features/setting/components/MessageInput.ts';
+import SettingItem from '@/features/setting/components/SettingItem.ts';
+import { UUID } from '@/common/types';
+
+class BreakMessageItem extends SettingItem {
+  constructor(messageId: UUID) {
+    super();
+
+    const messageInput = new MessageInput(newValue => settingState.updateBreakMessageById(messageId, newValue));
+    this.addChild(messageInput);
+    messageInput.mount(this.labelContainer);
+
+
+  }
+}
+
+export default BreakMessageItem;
