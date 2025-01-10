8a899d2abb335a6478dc38237dae71ae8e297ab8
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-10 14:05:16 +0330

    render short break messages using BreakMessageItem

--- src/features/setting/pages/BreakMessagesPage.ts
+++ src/features/setting/pages/BreakMessagesPage.ts
@@ -1,10 +1,24 @@
 import Component from '@/common/ui/base/Component.ts';
+import BreakMessageItem from '@/features/setting/components/BreakMessageItem.ts';
+import SettingSection from '@/features/setting/components/SettingSection.ts';
+import settingState from '@/core/state/SettingState.ts';
+import { UUID } from '@/common/types';
 
-class SettingsPage extends Component {
+class BreakMessagesPage extends Component {
   constructor() {
     super('div', 'flex flex-col space-y-4');
-    this.element.innerText = 'Break Messages Page';
+
+    const section = new SettingSection('Short Breaks');
+
+    for (const messageId in settingState.settings.shortBreakMessages) {
+      const messageItem = new BreakMessageItem(messageId as UUID);
+      section.addChild(messageItem);
+      messageItem.mount(section.settingContainer);
+    }
+
+    this.addChild(section);
+    section.mount(this);
   }
 }
 
-export default SettingsPage;
+export default BreakMessagesPage;
