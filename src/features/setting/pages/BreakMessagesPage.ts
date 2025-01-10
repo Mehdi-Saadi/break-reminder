42e0684d3e15ea25bb64e2e76b57e4662b69c728
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-10 14:29:02 +0330

    render short and long break sections

--- src/features/setting/pages/BreakMessagesPage.ts
+++ src/features/setting/pages/BreakMessagesPage.ts
@@ -2,15 +2,20 @@ import Component from '@/common/ui/base/Component.ts';
 import BreakMessageItem from '@/features/setting/components/BreakMessageItem.ts';
 import SettingSection from '@/features/setting/components/SettingSection.ts';
 import settingState from '@/core/state/SettingState.ts';
-import { UUID } from '@/common/types';
+import { BreakMessages, UUID } from '@/common/types';
 
 class BreakMessagesPage extends Component {
   constructor() {
     super('div', 'flex flex-col space-y-4');
 
-    const section = new SettingSection('Short Breaks');
+    this.createMessageSection('Short Breaks', settingState.settings.shortBreakMessages);
+    this.createMessageSection('Long Breaks', settingState.settings.longBreakMessages);
+  }
 
-    for (const messageId in settingState.settings.shortBreakMessages) {
+  private createMessageSection(sectionLabel: string, breakMessages: BreakMessages): void {
+    const section = new SettingSection(sectionLabel);
+
+    for (const messageId in breakMessages) {
       const messageItem = new BreakMessageItem(messageId as UUID);
       section.addChild(messageItem);
       messageItem.mount(section.settingContainer);
