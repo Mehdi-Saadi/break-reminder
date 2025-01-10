965334cf86e251ed8fff9ff3191a5cdf3f5ba874
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-10 14:21:04 +0330

    update styles

--- src/common/ui/buttons/SquareDeleteButton.ts
+++ src/common/ui/buttons/SquareDeleteButton.ts
@@ -5,7 +5,7 @@ class SquareDeleteButton extends SquareButton {
   constructor(onClick?: (event: MouseEvent) => void) {
     super(onClick);
 
-    this.element.classList.add('rounded-full', 'border', 'border-gray-300', 'text-red-800');
+    this.element.classList.add('rounded-full', 'border', 'border-gray-300', 'text-red-500');
     this.element.innerHTML = icon('delete', 'size-4');
   }
 }
