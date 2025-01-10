e8d6ad475ac79e33ef12ff2ff38f2223ce842221
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-10 18:37:38 +0330

    implement base timer

---
+++ src/core/timer.ts
@@ -0,0 +1,67 @@
+import { minutesToMilliseconds, secondsToMilliseconds } from '@/common/time.ts';
+import { Millisecond } from '@/common/types'
+import settingState from '@/core/state/SettingState.ts';
+import notify from '@/core/notification.ts';
+
+class Timer {
+  private workTimeout: NodeJS.Timeout | null = null;
+  private breakTimeout: NodeJS.Timeout | null = null;
+  private countOfShortWorks: number = 0;
+
+  constructor() {
+    this.setWorkTimeout();
+  }
+
+  private setWorkTimeout(): void {
+    this.workTimeout = setTimeout(this.takeBreak, minutesToMilliseconds(settingState.settings.shortWorkDuration));
+  }
+
+  private clearWorkTimeout(): void {
+    if (this.workTimeout) {
+      clearTimeout(this.workTimeout);
+    }
+  }
+
+  private takeBreak = (): void => {
+    this.countOfShortWorks++;
+
+    if (this.shouldTakeLongBreak()) {
+      this.takeLongBreak();
+    } else {
+      this.takeShortBreak();
+    }
+  };
+
+  private shouldTakeLongBreak(): boolean {
+    if (this.countOfShortWorks >= settingState.settings.countOfShortWorksForLongBreak) {
+      this.countOfShortWorks = 0;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private setBreakTimeout(ms: Millisecond): void {
+    this.breakTimeout = setTimeout(this.setWorkTimeout, ms);
+  }
+
+  private takeLongBreak(): void {
+    notify('Time for a long break!');
+
+    this.setBreakTimeout(secondsToMilliseconds(settingState.settings.longBreakDuration));
+  }
+
+  private takeShortBreak(): void {
+    notify('Time for a short break!');
+
+    this.setBreakTimeout(secondsToMilliseconds(settingState.settings.shortBreakDuration));
+  }
+
+  skip(): void {
+    console.log('skip');
+  }
+}
+
+const timer = new Timer();
+
+export default timer;
