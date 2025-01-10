7854ca52ac44a987d02777dea54799da5f9515ef
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-10 13:26:50 +0330

    add delete button

---
+++ src/common/ui/buttons/SquareDeleteButton.ts
@@ -0,0 +1,13 @@
+import SquareButton from '@/common/ui/buttons/SquareButton.ts';
+import icon from '@/common/ui/icons.ts';
+
+class SquareDeleteButton extends SquareButton {
+  constructor(onClick?: (event: MouseEvent) => void) {
+    super(onClick);
+
+    this.element.classList.add('rounded-full', 'border', 'border-gray-300', 'text-red-800');
+    this.element.innerHTML = icon('delete', 'size-4');
+  }
+}
+
+export default SquareDeleteButton;
