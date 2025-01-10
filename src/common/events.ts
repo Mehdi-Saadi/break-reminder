cb4bad2b5be7debd5f09b46d78f020836c375bfe
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-10 15:21:11 +0330

    create settingsStateEventBus

--- src/common/events.ts
+++ src/common/events.ts
@@ -2,3 +2,5 @@ import EventBus from '@/core/EventBus.ts';
 
 export type NavigationPage = 'settings' | 'break_messages' | 'advanced';
 export const pageNavEventBus = new EventBus<'navigate'>();
+
+export const settingStateEventBus = new EventBus<'change'>();
