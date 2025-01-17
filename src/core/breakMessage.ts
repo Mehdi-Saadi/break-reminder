0142d5fc6ee3360a92097d7b347a2824fa879123
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-17 16:14:49 +0330

    add BreakMessage class

---
+++ src/core/breakMessage.ts
@@ -0,0 +1,43 @@
+import settingState from '@/core/state/SettingState.ts';
+
+class BreakMessage {
+  private shortBreakMessageIndex: number = 0;
+  private longBreakMessageIndex: number = 0;
+
+  private getMessage(messages: string[], index: number): [string, number] {
+    if (messages.length < (index + 1)) {
+      index = 0;
+    }
+
+    const message = messages[index] || '';
+    index++;
+
+    return [message, index];
+  }
+
+  getShortBreakMessage(): string {
+    const [message, newIndex] = this.getMessage(
+      Object.values(settingState.settings.shortBreakMessages),
+      this.shortBreakMessageIndex,
+    );
+
+    this.shortBreakMessageIndex = newIndex;
+
+    return message;
+  }
+
+  getLongBreakMessage(): string {
+    const [message, newIndex] = this.getMessage(
+      Object.values(settingState.settings.longBreakMessages),
+      this.longBreakMessageIndex,
+    );
+
+    this.longBreakMessageIndex = newIndex;
+
+    return message;
+  }
+}
+
+const breakMessage = new BreakMessage();
+
+export default breakMessage;
