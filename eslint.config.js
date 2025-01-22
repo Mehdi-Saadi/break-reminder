076bf579c4d7bf3eab2dc6ea6a9919befce3a1fd
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-22 21:54:02 +0330

    update config

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,6 @@ export default [
     rules: {
       'semi': ['warn', 'always'], // Always require semicolons
       'quotes': ['warn', 'single', { avoidEscape: true }], // Prefer single quotes
-      'indent': ['warn', 2], // Enforce 2-space tab width
 
       // Possible Problems
       'no-duplicate-imports': ['warn'],
@@ -19,7 +18,6 @@ export default [
       'arrow-body-style': ['warn', 'as-needed'],
       'block-scoped-var': ['warn'],
       'curly': ['warn'], // Always use curly braces in control statements
-      'default-case': ['warn'], // Require a default case in switch statements
       'default-case-last': ['warn'], // Enforce default case to be the last
       'default-param-last': ['warn'],
       'eqeqeq': ['warn'], // Require strict equality (===)
