fb61d0ff8ccf209ea11eb2036f5d296dd13c4de8
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-24 17:46:19 +0330

    move ViewManager feature to app

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,8 @@
+import ViewManager from '@/features/setting/ViewManager.ts';
 import '@/app/window';
 import '@/features/tray';
+
+const app = document.getElementById('app');
+if (app) {
+  new ViewManager().mount(app);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,1 @@
-import ViewManager from '@/features/setting/ViewManager.ts';
-
-const app = document.getElementById('app');
-
-if (app) {
-
-  new ViewManager().mount(app);
-
-}
-
-// app scripts
 import '@/app';
