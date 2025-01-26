eb000442a8f7547cff367f460b3797c4f58250ee
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-26 10:10:27 +0330

    add generateRandomAlphabeticId helper

--- src/shared/crypto.ts
+++ src/shared/crypto.ts
@@ -1,2 +1,11 @@
-
 export type UUID = `${string}-${string}-${string}-${string}-${string}`;
+
+export const generateRandomAlphabeticId = (maxLength: number = 16): string => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  for (let i = 0; i < maxLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+};
