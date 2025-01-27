1ade1c26a9b31e86502caebd94e846d4166323a3
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-27 10:24:26 +0330

    create branded time types using Branded type

--- src/shared/time.ts
+++ src/shared/time.ts
@@ -1,9 +1,11 @@
-export type Minute = number;
-export type Second = number;
-export type Millisecond = number;
+import { Branded } from './types';
 
-export const minutesToSeconds = (minutes: Minute): Second => minutes * 60;
+export type Minute = Branded<number, 'Minute'>;
+export type Second = Branded<number, 'Second'>;
+export type Millisecond = Branded<number, 'Millisecond'>;
 
-export const secondsToMilliseconds = (seconds: Second): Millisecond => seconds * 1000;
+export const minutesToSeconds = (minutes: Minute): Second => minutes * 60 as Second;
+
+export const secondsToMilliseconds = (seconds: Second): Millisecond => seconds * 1000 as Millisecond;
 
 export const minutesToMilliseconds = (minutes: Minute): Millisecond => secondsToMilliseconds(minutesToSeconds(minutes));
