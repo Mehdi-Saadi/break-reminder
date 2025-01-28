6d84f5079cbf735aec258138aef9d453f8ed9989
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-28 10:07:50 +0330

    move break window scripts to break feature

--- src/app/window.ts
+++ src/app/window.ts
@@ -1,5 +1,4 @@
-import { getCurrentWindow, availableMonitors, Window } from '@tauri-apps/api/window';
-import { generateRandomAlphabeticId } from '@/shared/crypto';
+import { getCurrentWindow } from '@tauri-apps/api/window';
 import { UnlistenFn } from '@tauri-apps/api/event';
 
 /**
@@ -7,7 +6,7 @@ import { UnlistenFn } from '@tauri-apps/api/event';
  * 
  * Use this function once to prevent adding multiple close listeners
  */
-const hideWindowOnCloseClick = async (): Promise<UnlistenFn> =>
+const hideWindowOnClose = async (): Promise<UnlistenFn> =>
   await getCurrentWindow()
     .onCloseRequested(
       async (event): Promise<void> => {
@@ -16,41 +15,4 @@ const hideWindowOnCloseClick = async (): Promise<UnlistenFn> =>
       }
     );
 
-hideWindowOnCloseClick();
-
-const createBreakWindow = async (): Promise<void> => {
-  const monitors = await availableMonitors();
-
-  for (const monitor of monitors) {
-    const windowName = `break-reminder-${generateRandomAlphabeticId()}`;
-
-    const breakWindow = new Window(windowName, {
-      x: monitor.position.x,
-      y: monitor.position.y,
-      maximized: true,
-      decorations: true,
-      alwaysOnTop: true,
-      skipTaskbar: false,
-      resizable: false,
-      focus: true,
-      backgroundColor: '#000000',  // Ensure black background
-    });
-
-    // Handle window creation
-    await breakWindow.once('tauri://created', async () => {
-      console.log(`Window created: ${windowName} at ${monitor.position.x}, ${monitor.position.y}`);
-
-      // Wait a bit to ensure the window is fully rendered before closing
-      setTimeout(async () => {
-        await breakWindow.destroy();
-        console.log(`Window ${windowName} closed`);
-      }, 10000);
-    });
-
-    await breakWindow.once('tauri://error', (error) => {
-      console.error(`Error creating window ${windowName}:`, error);
-    });
-  }
-};
-
-// createBreakWindow();
+hideWindowOnClose();
