6018fecf452b223fdbe4b865a0b91d4d55cd1cc2
Mehdi <mehdi.0.saadi@gmail.com>
2025-01-28 14:07:30 +0330

    add ActionButton

---
+++ src/features/break/components/ActionButton.ts
@@ -0,0 +1,28 @@
+import Component from '@/shared/ui/base/Component.ts';
+
+class ActionButton extends Component {
+  constructor(label: string, protected onClick?: (event: MouseEvent) => void) {
+    super('button', 'flex items-center justify-center border border-gray-300 rounded-3xl w-24 h-10 mx-2 hover:bg-gray-300 hover:bg-opacity-10 active:bg-opacity-20');
+
+    this.element.setAttribute('type', 'button');
+
+    const span = document.createElement('span');
+    span.innerHTML = label;
+
+    this.element.appendChild(span);
+  }
+
+  protected onMounted(): void {
+    if (this.onClick) {
+      this.element.addEventListener('click', this.onClick);
+    }
+  }
+
+  protected onUnmounted(): void {
+    if (this.onClick) {
+      this.element.removeEventListener('click', this.onClick);
+    }
+  }
+}
+
+export default ActionButton;
