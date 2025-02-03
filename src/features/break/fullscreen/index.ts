ef21f667e2597ba6c23651107bbdf96716229c5f
Mehdi <mehdi.0.saadi@gmail.com>
2025-02-03 22:06:19 +0330

    use FullscreenBreak class

--- src/features/break/fullscreen/index.ts
+++ src/features/break/fullscreen/index.ts
@@ -1,7 +1,7 @@
 import { BreakWindowPayload } from '@/features/break/fullscreen/types';
+import { availableMonitors, Monitor } from '@tauri-apps/api/window';
 import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
 import { generateRandomAlphabeticId } from '@/shared/crypto';
-import { availableMonitors } from '@tauri-apps/api/window';
 import { secondsToMilliseconds } from '@/shared/time';
 import breakMessage from '@/features/break/message';
 import settingState from '@/shared/state/setting';
@@ -13,69 +13,89 @@ interface IWebviewWindowParams {
   query: BreakWindowPayload;
 }
 
-const createBreakWebviewWindow = (params: IWebviewWindowParams): WebviewWindow => {
-  const windowUniqueLabel = `break-window-${generateRandomAlphabeticId()}`;
-  const queryParams = params.query ? objectToQuery(params.query) : '';
+class FullscreenBreak {
+  private monitors: Monitor[] = [];
+  private largestMonitor: Monitor | null = null;
 
-  return new WebviewWindow(windowUniqueLabel, {
-    x: params.x,
-    y: params.y,
-    maximized: true,
-    decorations: !import.meta.env.PROD,
-    alwaysOnTop: true,
-    skipTaskbar: import.meta.env.PROD,
-    resizable: false,
-    focus: true,
-    visible: false,
-    transparent: true,
-    url: `/src/features/break/fullscreen/window/index.html${queryParams}`,
-  });
-};
+  constructor() {
+    this.initMonitors();
+  }
 
-// TODO: show break message only for largest monitor
-// const getLargestOrFirstMonitor = async (): Promise<Monitor> => {
-//   const monitors = await availableMonitors();
+  private initMonitors = async (): Promise<void> => {
+    this.monitors = await availableMonitors();
+  };
 
-//   return monitors.reduce((previousMonitor, currentMonitor) => {
-//     if (previousMonitor.scaleFactor === currentMonitor.scaleFactor) {
-//       return previousMonitor;
-//     } else if (previousMonitor.scaleFactor > currentMonitor.scaleFactor) {
-//       return previousMonitor;
-//     }
-//     return currentMonitor;
-//   });
-// };
+  // TODO: show break message only for largest monitor
+  private getLargestOrFirstMonitor(): Monitor {
+    if (!this.largestMonitor) {
+      this.largestMonitor = this.monitors.reduce((previousMonitor, currentMonitor) => {
+        if (previousMonitor.scaleFactor === currentMonitor.scaleFactor) {
+          return previousMonitor;
+        }
+        if (previousMonitor.scaleFactor > currentMonitor.scaleFactor) {
+          return previousMonitor;
+        }
+        return currentMonitor;
+      });
+    }
 
-const createFullscreenBreak = async (
-  breakWindowPayload: BreakWindowPayload
-): Promise<void> => {
-  const monitors = await availableMonitors();
+    return this.largestMonitor;
+  }
 
-  for (const monitor of monitors) {
-    const breakWindow = createBreakWebviewWindow({
-      x: monitor.position.x,
-      y: monitor.position.y,
-      query: breakWindowPayload,
-    });
+  private createWebviewWindow(
+    params: IWebviewWindowParams
+  ): WebviewWindow {
+    const windowUniqueLabel = `break-window-${generateRandomAlphabeticId()}`;
+    const queryParams = params.query ? objectToQuery(params.query) : '';
 
-    await breakWindow.once('tauri://created', async (): Promise<void> => await breakWindow.show());
-
-    await breakWindow.once('tauri://error', error => {
-      console.error('Error while creating window:', breakWindow, error);
+    return new WebviewWindow(windowUniqueLabel, {
+      x: params.x,
+      y: params.y,
+      maximized: true,
+      decorations: !import.meta.env.PROD,
+      alwaysOnTop: true,
+      skipTaskbar: import.meta.env.PROD,
+      resizable: false,
+      focus: true,
+      visible: false,
+      transparent: true,
+      url: `/src/features/break/fullscreen/window/index.html${queryParams}`,
     });
   }
-};
 
-export const createFullscreenShortBreak = async (): Promise<void> => {
-  await createFullscreenBreak({
-    message: breakMessage.getShortBreakMessage(),
-    timeout: secondsToMilliseconds(settingState.settings.shortBreakDuration),
-  });
-};
+  private async createFullscreenBreak(
+    breakWindowPayload: BreakWindowPayload
+  ): Promise<void> {
+    for (const monitor of this.monitors) {
+      const breakWindow = this.createWebviewWindow({
+        x: monitor.position.x,
+        y: monitor.position.y,
+        query: breakWindowPayload,
+      });
 
-export const createFullscreenLongBreak = async (): Promise<void> => {
-  await createFullscreenBreak({
-    message: breakMessage.getLongBreakMessage(),
-    timeout: secondsToMilliseconds(settingState.settings.longBreakDuration),
-  });
-};
+      await breakWindow.once('tauri://created', async (): Promise<void> => await breakWindow.show());
+
+      await breakWindow.once('tauri://error', error => {
+        console.error('Error while creating window:', breakWindow, error);
+      });
+    }
+  }
+
+  async shortBreak(): Promise<void> {
+    await this.createFullscreenBreak({
+      message: breakMessage.getShortBreakMessage(),
+      timeout: secondsToMilliseconds(settingState.settings.shortBreakDuration),
+    });
+  }
+
+  async longBreak(): Promise<void> {
+    await this.createFullscreenBreak({
+      message: breakMessage.getLongBreakMessage(),
+      timeout: secondsToMilliseconds(settingState.settings.longBreakDuration),
+    });
+  }
+}
+
+const fullscreenBreak = new FullscreenBreak();
+
+export default fullscreenBreak;
