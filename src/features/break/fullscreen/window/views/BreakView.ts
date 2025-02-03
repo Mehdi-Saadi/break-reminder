05620028ab7e97232c9542e04232bf8d9d3b8a50
Mehdi <mehdi.0.saadi@gmail.com>
2025-02-03 13:43:41 +0330

    cleanup code

--- src/features/break/fullscreen/window/views/BreakView.ts
+++ src/features/break/fullscreen/window/views/BreakView.ts
@@ -2,25 +2,38 @@ import ActionButton from '@/features/break/fullscreen/window/components/ActionBu
 import Component from '@/shared/ui/base/Component.ts';
 
 class BreakView extends Component {
+  private wrapper: HTMLDivElement;
+
   constructor() {
     super('div', 'w-screen h-screen flex items-center justify-center bg-black bg-opacity-80 text-gray-300 select-none');
 
-    const wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'flex flex-col items-center space-y-5');
+    this.wrapper = document.createElement('div');
+    this.wrapper.setAttribute('class', 'flex flex-col items-center space-y-5');
 
-    // message
+    this.createMessageSection();
+    // this.createTimerSection();
+    // this.createActionButtons();
+
+    this.element.appendChild(this.wrapper);
+  }
+
+  private createMessageSection(): void {
     const messageContainer = document.createElement('div');
     messageContainer.setAttribute('class', 'font-semibold text-4xl');
     messageContainer.innerHTML = 'Hello World!';
-    wrapper.appendChild(messageContainer);
+    this.wrapper.appendChild(messageContainer);
+  }
 
-    // todo timer
+  // TODO: implement a count down timer
+  private createTimerSection(): void {
     const timerWrapper = document.createElement('div');
     timerWrapper.setAttribute('class', 'text-sm opacity-0');
     timerWrapper.innerHTML = '00:12';
-    wrapper.appendChild(timerWrapper);
+    this.wrapper.appendChild(timerWrapper);
+  }
 
-    // todo actions
+  // TODO: implement action buttons for skipping and postponing break
+  private createActionButtons(): void {
     const actionWrapper = document.createElement('div');
     actionWrapper.setAttribute('class', 'flex items-center text-xs opacity-0');
     // postpone
@@ -31,9 +44,7 @@ class BreakView extends Component {
     const skipButton = new ActionButton('Skip');
     skipButton.mount(actionWrapper);
     this.addChild(skipButton);
-    wrapper.appendChild(actionWrapper);
-
-    this.element.appendChild(wrapper);
+    this.wrapper.appendChild(actionWrapper);
   }
 }
 
