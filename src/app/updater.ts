da0aafa268a6134aa775c718d1a0e3e4cbb4b34c
Mehdi <mehdi.0.saadi@gmail.com>
2025-02-08 07:03:59 +0330

    add error handling

--- src/app/updater.ts
+++ src/app/updater.ts
@@ -1,13 +1,23 @@
 import { check } from '@tauri-apps/plugin-updater';
 
 const installUpdateIfAvailable = async (): Promise<void> => {
-  const update = await check();
+  try {
+    const update = await check();
 
-  if (!update) {
-    return;
+    if (!update) {
+      return;
+    }
+
+    try {
+      await update.downloadAndInstall();
+    } catch (error) {
+      alert('There was an error while downloading updates..');
+      console.error(error);
+    }
+  } catch (error) {
+    alert('There was an error while checking updates..');
+    console.error(error);
   }
-
-  await update.downloadAndInstall();
 };
 
 installUpdateIfAvailable();
