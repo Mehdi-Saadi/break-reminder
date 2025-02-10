eab1d19e7eb52a0306f6c0bd1dfcce7b2b4ebfbb
Mehdi <mehdi.0.saadi@gmail.com>
2025-02-10 22:14:18 +0330

    import .wav files, so vite can understand to put them in assets

--- src/features/break/audio/index.ts
+++ src/features/break/audio/index.ts
@@ -1,4 +1,6 @@
+import preBreakAudio from '@/assets/audio/on_pre_break.wav';
+import stopBreakAudio from '@/assets/audio/on_stop_break.wav';
 import { playAudio } from '@/shared/audio';
 
-export const playPreBreakAudio = (): Promise<void> => playAudio('/src/assets/audio/on_pre_break.wav');
-export const playStopBreakAudio = (): Promise<void> => playAudio('/src/assets/audio/on_stop_break.wav');
+export const playPreBreakAudio = (): Promise<void> => playAudio(preBreakAudio);
+export const playStopBreakAudio = (): Promise<void> => playAudio(stopBreakAudio);
