7e2c6b2e37095b81ae47bc230574eac8f30bce05
Mehdi <mehdi.0.saadi@gmail.com>
2025-02-11 11:30:53 +0330

    types.d.ts -> communication.ts

--- src/features/break/fullscreen/communication.ts
+++ src/features/break/fullscreen/communication.ts
@@ -4,3 +4,8 @@ export interface BreakWindowPayload {
   message?: string;
   timeout: Millisecond;
 }
+
+export const BREAK_WINDOW_EVENT = Object.freeze({
+  skip: 'break-window-skip',
+  postpone: 'break-window-postpone',
+});
