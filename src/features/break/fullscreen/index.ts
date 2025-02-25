399f66685fa45fa7bcb727beb3fb67647759c2f2
Mehdi <mehdi.0.saadi@gmail.com>
2025-02-25 13:32:01 +0330

    get available monitors each time creating window, to detect new connected monitors

--- src/features/break/fullscreen/index.ts
+++ src/features/break/fullscreen/index.ts
@@ -14,32 +14,19 @@ interface IWebviewWindowParams {
 }
 
 class FullscreenBreak {
-  private monitors: Monitor[] = [];
-  private largestMonitor: Monitor | null = null;
-
-  constructor() {
-    this.initMonitors();
-  }
-
-  private initMonitors = async (): Promise<void> => {
-    this.monitors = await availableMonitors();
-  };
-
   // TODO: show break message only for largest monitor
-  private getLargestOrFirstMonitor(): Monitor {
-    if (!this.largestMonitor) {
-      this.largestMonitor = this.monitors.reduce((previousMonitor, currentMonitor) => {
-        if (previousMonitor.scaleFactor === currentMonitor.scaleFactor) {
-          return previousMonitor;
-        }
-        if (previousMonitor.scaleFactor > currentMonitor.scaleFactor) {
-          return previousMonitor;
-        }
-        return currentMonitor;
-      });
-    }
+  private async getLargestOrFirstMonitor(): Promise<Monitor> {
+    const monitors = await availableMonitors();
 
-    return this.largestMonitor;
+    return monitors.reduce((previousMonitor, currentMonitor) => {
+      if (previousMonitor.scaleFactor === currentMonitor.scaleFactor) {
+        return previousMonitor;
+      }
+      if (previousMonitor.scaleFactor > currentMonitor.scaleFactor) {
+        return previousMonitor;
+      }
+      return currentMonitor;
+    });
   }
 
   private createWebviewWindow(
@@ -66,7 +53,9 @@ class FullscreenBreak {
   private async createFullscreenBreak(
     breakWindowPayload: BreakWindowPayload
   ): Promise<void> {
-    for (const monitor of this.monitors) {
+    const monitors = await availableMonitors();
+
+    for (const monitor of monitors) {
       const breakWindow = this.createWebviewWindow({
         x: monitor.position.x,
         y: monitor.position.y,
