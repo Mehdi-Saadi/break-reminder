8777001179b23066d97e3ce01015cef2a47d10ce
Mehdi <mehdi.0.saadi@gmail.com>
2025-03-22 00:11:28 +0330

    remove OS plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,6 @@ mod commands;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::init(
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
