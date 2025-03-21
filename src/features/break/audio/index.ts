9b23d084a295741ec4caf21ec2c19fcc81a220e6
Mehdi <mehdi.0.saadi@gmail.com>
2025-03-22 01:04:37 +0330

    implement break audio functionality

--- src/features/break/audio/index.ts
+++ src/features/break/audio/index.ts
@@ -1,6 +1,36 @@
 import preBreakAudio from '@/assets/audio/on_pre_break.wav';
+import settingState from '@/shared/state/setting';
 import stopBreakAudio from '@/assets/audio/on_stop_break.wav';
+import { invoke } from '@tauri-apps/api/core';
 import { playAudio } from '@/shared/audio';
 
-export const playPreBreakAudio = (): Promise<void> => playAudio(preBreakAudio);
-export const playStopBreakAudio = (): Promise<void> => playAudio(stopBreakAudio);
+const playPreBreakAudio = (): Promise<void> => playAudio(preBreakAudio);
+const playStopBreakAudio = (): Promise<void> => playAudio(stopBreakAudio);
+
+class BreakAudio {
+  private preBreakAudioHasBeenPlayed: boolean = false;
+
+  async playPreBreakAudio(): Promise<void> {
+    if (
+      settingState.settings.audibleAlert &&
+      !await invoke('check_focused_window_maximized')
+    ) {
+      await playPreBreakAudio();
+      this.preBreakAudioHasBeenPlayed = true;
+    }
+  }
+
+  async playStopBreakAudio(): Promise<void> {
+    if (
+      settingState.settings.audibleAlert &&
+      this.preBreakAudioHasBeenPlayed
+    ) {
+      await playStopBreakAudio();
+      this.preBreakAudioHasBeenPlayed = false;
+    }
+  }
+}
+
+const breakAudio = new BreakAudio();
+
+export default breakAudio;
