e8f795fda43d59b8ff52f27db825924d5057a4d6
Mehdi <mehdi.0.saadi@gmail.com>
2025-04-01 03:18:29 +0330

    remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-import '@/core';
+import ViewManager from '@/modules/setting/ViewManager.ts';
+import browserShortcut from '@/shared/browserShortcut';
+import '@/modules/window';
+import '@/modules/timer';
+import '@/modules/updater';
+import '@/modules/autostart';
+import '@/modules/tray';
+
+browserShortcut.disableAllInProd();
+
+const app = document.getElementById('app');
+if (app) {
+  new ViewManager().mount(app);
+}
