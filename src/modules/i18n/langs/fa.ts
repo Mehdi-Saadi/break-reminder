550acdd90e72586f000c30386d7c16e207cecc66
Mehdi <mehdi.0.saadi@gmail.com>
2025-05-08 21:33:11 +0330

    add persian translation

---
+++ src/modules/i18n/langs/fa.ts
@@ -0,0 +1,26 @@
+import { Words } from '@/modules/i18n/types';
+
+const fa: Words = {
+  settings: 'تنظیمات',
+  breakMessages: 'پیام‌های استراحت',
+  advanced: 'پیشرفته',
+  shortBreaks: 'استراحت‌های کوتاه',
+  longBreaks: 'استراحت‌های طولانی',
+  options: 'گزینه‌ها',
+  workDurationInMinutes: 'مدت زمان کار (به دقیقه)',
+  breakDurationInSeconds: 'مدت زمان استراحت (به ثانیه)',
+  countOfShortWorksForLongBreak: 'تعداد کارها برای یک استراحت طولانی',
+  timeToPrepareForBreakInSeconds: 'زمان آماده شدن برای استراحت (به ثانیه)',
+  strictBreakNoWayToSkip: 'استراحت مطلق (به هیچ وجه نمی‌توان از استراحت‌ها صرف نظر کرد)',
+  doNotDisturb: 'مزاحم نشوید',
+  doNotDisturbInfo: 'اگر پنجره فعال در حالت تمام صفحه است، از استراحت صرف نظر میشود',
+  notification: 'اعلان',
+  notificationInfo: 'نمایش اعلان سیستمی قبل از استراحت‌ها',
+  audibleAlert: 'هشدار صوتی',
+  audibleAlertInfo: 'پخش هشدار صوتی در پایان زمان استراحت',
+  darkMode: 'حالت تاریک',
+  runOnStartup: 'اجرا در هنگام راه‌اندازی',
+  quit: 'ترک کردن',
+};
+
+export default fa;
