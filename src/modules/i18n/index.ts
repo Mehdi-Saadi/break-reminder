603acf7767afccdbd26549034f0f6c71d9c11ce5
Mehdi <mehdi.0.saadi@gmail.com>
2025-05-08 21:53:52 +0330

    implement translation function

---
+++ src/modules/i18n/index.ts
@@ -0,0 +1,21 @@
+import en from '@/modules/i18n/langs/en';
+import fa from '@/modules/i18n/langs/fa';
+import settingState from '@/shared/state/setting.ts';
+import { KeyWord } from '@/modules/i18n/types';
+
+const { language } = settingState.value;
+
+const translations = {
+  en,
+  fa,
+} as const;
+
+settingState.subscribe((newValue): void => {
+  if (newValue.language !== language) {
+    window.location.reload();
+  }
+});
+
+const t = (key: KeyWord): string => translations[language]?.[key] ?? key;
+
+export default t;
