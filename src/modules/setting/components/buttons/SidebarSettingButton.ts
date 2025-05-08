8c4b4f9142b04e21b1b9424b7c65fd90daf821b3
Mehdi <mehdi.0.saadi@gmail.com>
2025-05-08 22:20:46 +0330

    add translation

--- src/modules/setting/components/buttons/SidebarSettingButton.ts
+++ src/modules/setting/components/buttons/SidebarSettingButton.ts
@@ -1,12 +1,13 @@
 import SidebarNavigationButton from '@/modules/setting/components/buttons/SidebarNavigationButton.ts';
 import icon from '@/shared/ui/icons.ts';
+import t from '@/modules/i18n';
 
 class SidebarSettingButton extends SidebarNavigationButton {
   constructor() {
     super(
       'settings',
       icon('gear', 'size-5 me-1.5'),
-      'Settings',
+      t('settings'),
     );
 
     // activate default page
