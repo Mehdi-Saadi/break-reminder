8785526d791f017eb2c8dcbd7d862355e0686d16
Mehdi <mehdi.0.saadi@gmail.com>
2025-05-09 14:38:11 +0330

    implement general view

---
+++ src/modules/setting/views/GeneralView.ts
@@ -0,0 +1,58 @@
+import CheckboxSettingItem from '@/modules/setting/components/CheckboxSettingItem.ts';
+import Component from '@/shared/ui/base/Component.ts';
+import settingState from '@/shared/state/setting';
+import t from '@/modules/i18n';
+import { IconName } from '@/shared/ui/icons.ts';
+
+class GeneralView extends Component {
+  constructor() {
+    super('div', 'flex flex-col space-y-5 p-4 text-sm');
+
+    this.createSettingItems();
+  }
+
+  private createSettingItems(): void {
+    this.createCheckboxSettingItem(
+      t('darkMode'),
+      'darkMode',
+      settingState.value.darkMode,
+      (newValue) => {
+        settingState.value = {
+          ...settingState.value,
+          darkMode: newValue
+        };
+      }
+    );
+    this.createCheckboxSettingItem(
+      t('runOnStartup'),
+      'presentToAll',
+      settingState.value.autostart,
+      (newValue) => {
+        settingState.value = {
+          ...settingState.value,
+          autostart: newValue
+        };
+      }
+    );
+  }
+
+  private createCheckboxSettingItem(
+    label: string,
+    iconName: IconName,
+    initialState: boolean,
+    onChange: (newValue: boolean) => void
+  ): void {
+    const settingItem = new CheckboxSettingItem(
+      label,
+      '',
+      iconName,
+      initialState,
+      onChange
+    ).create();
+
+    this.addChild(settingItem);
+    settingItem.mount(this);
+  }
+}
+
+export default GeneralView;
