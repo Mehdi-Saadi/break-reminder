23149bc8e0597fd1949e94555b44a84b8cfcd877
Mehdi <mehdi.0.saadi@gmail.com>
2025-05-23 13:05:08 +0330

    implement sidebar about button

---
+++ src/modules/setting/components/buttons/SidebarAboutButton.ts
@@ -0,0 +1,18 @@
+import SidebarNavigationButton from '@/modules/setting/components/buttons/SidebarNavigationButton.ts';
+import icon from '@/shared/ui/icons.ts';
+import t from '@/modules/i18n';
+
+class SidebarAboutButton extends SidebarNavigationButton {
+  constructor() {
+    super(
+      'about',
+      icon('info', 'size-5 me-1.5'),
+      t('about'),
+    );
+
+    // activate default page
+    this.setActive();
+  }
+}
+
+export default SidebarAboutButton;
