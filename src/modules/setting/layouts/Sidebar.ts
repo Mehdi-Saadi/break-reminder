bff86db3bd349cbe2a8d0183a8231019b387b295
Mehdi <mehdi.0.saadi@gmail.com>
2025-05-23 13:05:50 +0330

    add about button to sidebar

--- src/modules/setting/layouts/Sidebar.ts
+++ src/modules/setting/layouts/Sidebar.ts
@@ -1,19 +1,31 @@
+import Component from '@/shared/ui/base/Component.ts';
+import SidebarAboutButton from '@/modules/setting/components/buttons/SidebarAboutButton.ts';
 import SidebarBehaviorButton from '@/modules/setting/components/buttons/SidebarBehaviorButton.ts';
 import SidebarGeneralButton from '@/modules/setting/components/buttons/SidebarGeneralButton.ts';
-import Component from '@/shared/ui/base/Component.ts';
 import SidebarNotificationAlertButton from '@/modules/setting/components/buttons/SidebarNotificationAlertButton.ts';
 import SidebarBreakMessagesButton from '@/modules/setting/components/buttons/SidebarBreakMessagesButton.ts';
 import SidebarBreakScheduleButton from '@/modules/setting/components/buttons/SidebarBreakScheduleButton.ts';
 
 class Sidebar extends Component {
   constructor() {
-    super('div', 'h-full w-1/3 fixed top-0 start-0 bg-inherit p-5 space-y-1');
+    super('div', 'h-full w-full bg-inherit p-5 flex flex-col justify-between');
 
-    new SidebarGeneralButton().mount(this);
-    new SidebarBreakScheduleButton().mount(this);
-    new SidebarBehaviorButton().mount(this);
-    new SidebarNotificationAlertButton().mount(this);
-    new SidebarBreakMessagesButton().mount(this);
+    const mainButtonWrapper = document.createElement('div');
+    mainButtonWrapper.setAttribute('class', 'space-y-1 w-full');
+
+    new SidebarGeneralButton().mount(mainButtonWrapper);
+    new SidebarBreakScheduleButton().mount(mainButtonWrapper);
+    new SidebarBehaviorButton().mount(mainButtonWrapper);
+    new SidebarNotificationAlertButton().mount(mainButtonWrapper);
+    new SidebarBreakMessagesButton().mount(mainButtonWrapper);
+
+    const aboutButtonWrapper = document.createElement('div');
+    aboutButtonWrapper.setAttribute('class', 'w-full');
+
+    new SidebarAboutButton().mount(aboutButtonWrapper);
+
+    this.element.appendChild(mainButtonWrapper);
+    this.element.appendChild(aboutButtonWrapper);
   }
 }
 
