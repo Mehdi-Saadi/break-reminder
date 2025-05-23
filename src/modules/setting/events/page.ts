08ffe528e09d9edc273bc0ef1576c530c30efe3e
Mehdi <mehdi.0.saadi@gmail.com>
2025-05-23 13:06:27 +0330

    add about navigation page type

--- src/modules/setting/events/page.ts
+++ src/modules/setting/events/page.ts
@@ -1,4 +1,4 @@
 import EventBus from '@/shared/event/EventBus.ts';
 
-export type NavigationPage = 'general' | 'break_schedule' | 'behavior' | 'notification' | 'break_messages';
+export type NavigationPage = 'general' | 'break_schedule' | 'behavior' | 'notification' | 'break_messages' | 'about';
 export const pageNavEventBus = new EventBus<'navigate'>();
