080bf62a3fd1b69b84c1de716897b1dd2ab4b48c
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 22:10:36 +0330

    implement notif button

---
+++ src/components/sidebar/NotifButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavButton
+    :to="{ name: 'notification-alerts' }"
+    :title="t('notificationsAndAlerts')"
+    :ui="{ icon: 'size-5 me-1.5' }"
+    icon="lucide:bell"
+  />
+</template>
+
+<script setup lang="ts">
+import NavButton from '@/components/sidebar/NavButton.vue';
+import { useT } from '@/composables/t';
+
+const t = useT();
+</script>
