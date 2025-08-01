2de1d5cc4a727a6405874a11487e9117ce9c4efd
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 14:09:41 +0330

    move store creation to stores folder

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,15 @@ import '@/assets/styles/main.css';
 
 import App from '@/App.vue';
 import router from '@/router';
+import store from '@/stores';
 import ui from '@nuxt/ui/vue-plugin';
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 
 const app = createApp(App);
-const pinia = createPinia();
 
 // modules
 app.use(router);
 app.use(ui);
-app.use(pinia);
+app.use(store);
 
 app.mount('#app');
