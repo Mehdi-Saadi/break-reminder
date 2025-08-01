d5b059be3d11503a79c807813c245c0947b19403
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 20:45:20 +0330

    restore time types

---
+++ src/types/time.ts
@@ -0,0 +1,5 @@
+import { Branded } from '@/types/common';
+
+export type Minute = Branded<number, 'Minute'>;
+export type Second = Branded<number, 'Second'>;
+export type Millisecond = Branded<number, 'Millisecond'>;
