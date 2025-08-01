9ec9c266047ea818c09ad21916c844b04ad5f734
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 20:48:53 +0330

    restore setting types

---
+++ src/types/setting.ts
@@ -0,0 +1,34 @@
+import { Minute, Second } from '@/types/time';
+import { UUID } from 'node:crypto';
+
+export interface Settings {
+  // short break
+  shortBreakDuration: Second;
+  shortWorkDuration: Minute;
+
+  // long break
+  longBreakDuration: Second;
+  countOfShortWorksForLongBreak: number;
+
+  // options
+  timeToPrepareForBreak: Second;
+  strictBreak: boolean;
+  allowPostponingBreaks: boolean;
+  postponeDuration: Minute;
+
+  // advanced
+  autostart: boolean;
+  doNotDisturb: boolean;
+  notification: boolean;
+  audibleAlert: boolean;
+  selectedAudio: string | null;
+  smartPause: boolean;
+  screensaver: boolean;
+  darkMode: boolean;
+
+  shortBreakMessages: BreakMessages;
+  longBreakMessages: BreakMessages;
+  language: 'en' | 'fa';
+}
+
+export type BreakMessages = Record<UUID, string>;
