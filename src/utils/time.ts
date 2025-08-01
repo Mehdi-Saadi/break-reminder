ac69103c6516ad63c7824ad5820350c7979e5a35
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 20:45:39 +0330

    restore time utils

---
+++ src/utils/time.ts
@@ -0,0 +1,11 @@
+import { Millisecond, Minute, Second } from '@/types/time';
+
+export const minutesToSeconds = (minutes: Minute): Second => minutes * 60 as Second;
+
+export const secondsToMilliseconds = (seconds: Second): Millisecond => seconds * 1000 as Millisecond;
+
+export const minutesToMilliseconds = (minutes: Minute): Millisecond => secondsToMilliseconds(minutesToSeconds(minutes));
+
+export const millisecondsToSeconds = (milliseconds: Millisecond): Second => Math.floor(milliseconds / 1000) as Second;
+
+export const secondsToMinutes = (seconds: Second): Minute => Math.floor(seconds / 60) as Minute;
