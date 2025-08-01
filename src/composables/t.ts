2ccc5b1d39b558477792a2ecf76177fcc8a8068c
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 22:22:36 +0330

    change t params types

--- src/composables/t.ts
+++ src/composables/t.ts
@@ -1,13 +1,11 @@
 import { KeyWord } from '@/i18n/types';
 import { useI18n } from 'vue-i18n';
 
-type OmitFirst<T extends any[]> = T extends [any, ...infer R] ? R : never;
-
 export const useT = () => {
   const { t } = useI18n();
 
   return (
     key: KeyWord,
-    ...params: OmitFirst<Parameters<typeof t>>
+    ...params: any[]
   ): string => t(key, ...params);
 };
