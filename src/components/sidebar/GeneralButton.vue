56eb5602a8b1dd1d510bc91069d0e51c05519296
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 21:46:58 +0330

    implement general button

---
+++ src/components/sidebar/GeneralButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavButton
+    :to="{ name: 'general' }"
+    :title="t('general')"
+    :ui="{ icon: 'size-5 me-1.5' }"
+    icon="ic:baseline-settings"
+  />
+</template>
+
+<script setup lang="ts">
+import NavButton from '@/components/sidebar/NavButton.vue';
+import { useT } from '@/composables/t';
+
+const t = useT();
+</script>
