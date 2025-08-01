5fe9db871317831c1275f8f6d434f2ed58144be1
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 20:56:48 +0330

    restore notification util

---
+++ src/utils/notification.ts
@@ -0,0 +1,25 @@
+import {
+  Options,
+  isPermissionGranted,
+  requestPermission,
+  sendNotification,
+} from '@tauri-apps/plugin-notification';
+
+const checkPermission = async (): Promise<boolean> => {
+  if (!(await isPermissionGranted())) {
+    return (await requestPermission()) === 'granted';
+  }
+  return true;
+};
+
+const notify = async (options: Options | string): Promise<void> => {
+  if (!(await checkPermission())) {
+    const message: string = typeof options === 'string' ? options : options.title;
+
+    return alert(message);
+  }
+
+  sendNotification(options);
+};
+
+export default notify;
