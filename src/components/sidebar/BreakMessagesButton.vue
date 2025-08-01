558d0dd83a7b549d88a27335b27ad09cb09b272c
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 22:13:23 +0330

    implement break messages button

---
+++ src/components/sidebar/BreakMessagesButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavButton
+    :to="{ name: 'break-messages' }"
+    :title="t('breakMessages')"
+    :ui="{ icon: 'size-5 me-1.5' }"
+    icon="lucide:message-circle"
+  />
+</template>
+
+<script setup lang="ts">
+import NavButton from '@/components/sidebar/NavButton.vue';
+import { useT } from '@/composables/t';
+
+const t = useT();
+</script>
