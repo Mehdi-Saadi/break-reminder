efca2859356c1c0218aae88761400366242a6511
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 22:19:15 +0330

    implement base routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,45 @@
+import App from '@/layouts/App.vue';
+import General from '@/views/General.vue';
 import { RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [];
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: App,
+    redirect: { name: 'general' },
+    children: [
+      {
+        path: 'general',
+        name: 'general',
+        component: General,
+      },
+      {
+        path: 'break-schedule',
+        name: 'break-schedule',
+        component: General,
+      },
+      {
+        path: 'behavior',
+        name: 'behavior',
+        component: General,
+      },
+      {
+        path: 'notification-alerts',
+        name: 'notification-alerts',
+        component: General,
+      },
+      {
+        path: 'break-messages',
+        name: 'break-messages',
+        component: General,
+      },
+      {
+        path: 'about',
+        name: 'about',
+        component: General,
+      },
+    ],
+  },
+];
 
 export default routes;
