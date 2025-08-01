ffe6846a814e528d9424d8a0739c9691d7966eb5
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 22:06:45 +0330

    implement behavior button

---
+++ src/components/sidebar/BehaviorButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavButton
+    :to="{ name: 'behavior' }"
+    :title="t('behavior')"
+    :ui="{ icon: 'size-5 me-1.5' }"
+    icon="lucide:brain"
+  />
+</template>
+
+<script setup lang="ts">
+import NavButton from '@/components/sidebar/NavButton.vue';
+import { useT } from '@/composables/t';
+
+const t = useT();
+</script>
