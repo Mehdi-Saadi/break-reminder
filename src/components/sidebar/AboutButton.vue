d87fbddd49f4aeb6d3a5a3bcbafa87e23f8e9c09
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 22:17:56 +0330

    implement about button

---
+++ src/components/sidebar/AboutButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavButton
+    :to="{ name: 'about' }"
+    :title="t('aboutSoftware')"
+    icon="lucide:info"
+  />
+</template>
+
+<script setup lang="ts">
+import NavButton from '@/components/sidebar/NavButton.vue';
+import { useT } from '@/composables/t';
+
+const t = useT();
+</script>
