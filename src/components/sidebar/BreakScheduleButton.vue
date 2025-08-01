066730261d75019149004e7095f38cd3be78cca2
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 21:52:11 +0330

    implement break schedule button

---
+++ src/components/sidebar/BreakScheduleButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavButton
+    :to="{ name: 'break-schedule' }"
+    :title="t('breakSchedule')"
+    :ui="{ icon: 'size-5 me-1.5' }"
+    icon="ic:baseline-access-alarm"
+  />
+</template>
+
+<script setup lang="ts">
+import NavButton from '@/components/sidebar/NavButton.vue';
+import { useT } from '@/composables/t';
+
+const t = useT();
+</script>
