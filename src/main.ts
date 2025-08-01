2bae845bbf8102d39450f9e04e08754a8bf7dc84
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 14:07:19 +0330

    install and configure pinia

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import App from '@/App.vue';
 import router from '@/router';
 import ui from '@nuxt/ui/vue-plugin';
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
+const pinia = createPinia();
 
 // modules
 app.use(router);
 app.use(ui);
+app.use(pinia);
 
 app.mount('#app');
