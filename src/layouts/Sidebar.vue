ebf1e60f7a3e16057b8a811f0a4167821035b348
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 22:18:21 +0330

    implement sidebar

---
+++ src/layouts/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="h-full w-1/3 fixed bg-inherit p-5 flex flex-col justify-between">
+    <!-- main buttons -->
+    <div class="space-y-1 w-full">
+      <GeneralButton />
+      <BreakScheduleButton />
+      <BehaviorButton />
+      <NotifButton />
+      <BreakMessagesButton />
+    </div>
+
+    <!-- about software -->
+    <div class="w-full">
+      <AboutButton />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GeneralButton from '@/components/sidebar/GeneralButton.vue';
+import BreakScheduleButton from '@/components/sidebar/BreakScheduleButton.vue';
+import BehaviorButton from '@/components/sidebar/BehaviorButton.vue';
+import NotifButton from '@/components/sidebar/NotifButton.vue';
+import BreakMessagesButton from '@/components/sidebar/BreakMessagesButton.vue';
+import AboutButton from '@/components/sidebar/AboutButton.vue';
+</script>
