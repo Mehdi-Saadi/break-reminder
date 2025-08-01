a6f2b0f43c708e9cd0bea075fe336b462d85c121
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-01 22:18:45 +0330

    implement app layout using sidebar

---
+++ src/layouts/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    :class="{ dark: darkMode }"
+    class="size-full min-h-screen grid grid-cols-3 bg-[#f3f3f3] text-gray-800 dark:bg-[#202020] dark:text-white select-none"
+  >
+    <div class="col-span-1">
+      <Sidebar />
+    </div>
+
+    <div class="col-span-2 py-6 ps-3 pe-7">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Sidebar from '@/layouts/Sidebar.vue';
+import { useSettingStore } from '@/stores/setting';
+import { computed } from 'vue';
+
+const settingStore = useSettingStore();
+
+const darkMode = computed(() => settingStore.settings.darkMode);
+</script>
