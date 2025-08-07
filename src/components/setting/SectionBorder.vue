ed058e728a096c8b494473961856fe494ce1abe9
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-07 20:51:36 +0330

    implement base setion border

---
+++ src/components/setting/SectionBorder.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <div class="relative pt-2">
+    <div class="border border-gray-300 rounded-md p-4 text-sm">
+      <!-- label -->
+      <span class="absolute top-0 start-2 text-xs font-semibold bg-[#f3f3f3] dark:bg-[#202020] px-1">
+        {{ label }}
+      </span>
+
+      <div class="flex flex-col space-y-3">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
