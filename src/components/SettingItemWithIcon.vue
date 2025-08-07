e188b29355c949e4f8b12847657e6bd327c07548
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-07 19:25:38 +0330

    implement setting item design

--- src/components/SettingItemWithIcon.vue
+++ src/components/SettingItemWithIcon.vue
@@ -7,23 +7,25 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex items-center justify-between">
+  <div class="flex items-center">
     <!-- label -->
-    <div class="flex items-center space-x-1">
+    <div class="grow pe-4 pb-0.5 flex items-center space-x-1">
       <UIcon
         :name="icon"
+        class="size-5"
       />
 
-      <div>
-        <div>
+      <div class="flex flex-col">
+        <div class="font-semibold">
           {{ title }}
         </div>
-        <div>
+        <div class="text-xs text-[#5c5c5c] dark:text-[#cccccc]">
           {{ description }}
         </div>
       </div>
     </div>
 
+    <!-- setting content -->
     <div>
       <slot />
     </div>
