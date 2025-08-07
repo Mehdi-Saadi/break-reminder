cb6f319eb3f1c1cc32ea1b37f006db65fd0ac584
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-07 21:23:31 +0330

    implement base Behavior view

---
+++ src/views/Behavior.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Item from '@/components/setting/Item.vue';
+import { useT } from '@/composables/t';
+import { useSettingStore } from '@/stores/setting';
+import { storeToRefs } from 'pinia';
+
+const t = useT();
+
+const { settings } = storeToRefs(useSettingStore());
+</script>
+
+<template>
+  <div class="flex flex-col space-y-5 p-4 text-sm">
+    <Item
+      :title="t('strictBreak')"
+      :description="t('strictBreakInfo')"
+      icon="lucide:lock"
+    >
+      <UCheckbox
+        v-model="settings.strictBreak"
+        size="xl"
+      />
+    </Item>
+  </div>
+</template>
