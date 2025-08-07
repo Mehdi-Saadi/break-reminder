01e32699d9371194103b235e0dbf426889b0338d
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-07 19:07:24 +0330

    implement base general view

---
+++ src/views/General.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import SettingItemWithIcon from '@/components/SettingItemWithIcon.vue';
+import { useT } from '@/composables/t';
+
+const t = useT();
+</script>
+
+<template>
+  <div class="flex flex-col space-y-5 p-4 text-sm">
+    <SettingItemWithIcon
+      :title="t('language')"
+      icon="lucide:languages"
+    >
+      test
+    </SettingItemWithIcon>
+  </div>
+</template>
