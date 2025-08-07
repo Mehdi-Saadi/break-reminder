8925fa50ad468c7bf3abdf92323ccd69b66e5293
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-07 19:07:12 +0330

    implement base setting item with icon

---
+++ src/components/SettingItemWithIcon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  icon: string;
+  description?: string;
+}>();
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <!-- label -->
+    <div class="flex items-center space-x-1">
+      <UIcon
+        :name="icon"
+      />
+
+      <div>
+        <div>
+          {{ title }}
+        </div>
+        <div>
+          {{ description }}
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
