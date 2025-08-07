a08ecacace50fb5450f832711c4b829dd4a54720
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-07 21:29:26 +0330

    implement do not disturb and time to prepare for break setting items

--- src/views/Behavior.vue
+++ src/views/Behavior.vue
@@ -11,6 +11,7 @@ const { settings } = storeToRefs(useSettingStore());
 
 <template>
   <div class="flex flex-col space-y-5 p-4 text-sm">
+    <!-- strick break -->
     <Item
       :title="t('strictBreak')"
       :description="t('strictBreakInfo')"
@@ -19,6 +20,31 @@ const { settings } = storeToRefs(useSettingStore());
       <UCheckbox
         v-model="settings.strictBreak"
         size="xl"
+      />
+    </Item>
+
+    <!-- do not disturb -->
+    <Item
+      :title="t('doNotDisturb')"
+      :description="t('doNotDisturbInfo')"
+      icon="lucide:octagon-x"
+    >
+      <UCheckbox
+        v-model="settings.doNotDisturb"
+        size="xl"
+      />
+    </Item>
+
+    <!-- time to prepare for a break -->
+    <Item
+      :title="t('timeToPrepareForBreakInSeconds')"
+      icon="lucide:hourglass"
+    >
+      <UInputNumber
+        v-model="settings.timeToPrepareForBreak"
+        :max="900"
+        :min="1"
+        class="max-w-32"
       />
     </Item>
   </div>
