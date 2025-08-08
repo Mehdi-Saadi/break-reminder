b9db4e8e7f89f35aac1fbd3c6e497126469b1fe6
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-08 13:13:13 +0330

    implement break message item component

---
+++ src/components/BreakMessageItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const model = defineModel<string>();
+
+const emit = defineEmits<{
+  (e: 'delete'): void;
+}>();
+</script>
+
+<template>
+  <div class="flex items-center space-x-4">
+    <UInput
+      v-model="model"
+      class="grow"
+    />
+
+    <UButton
+      class="rounded-full"
+      variant="subtle"
+      color="error"
+      icon="lucide:trash-2"
+      @click="emit('delete')"
+    />
+  </div>
+</template>
