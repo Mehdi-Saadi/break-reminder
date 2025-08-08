cd97c96c524e3f018d3290e545f97cfea43c9ba1
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-08 14:02:12 +0330

    remove unused buttons, use UButton for nav buttons

---
+++ src/components/layout/NavButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const props = defineProps<{
+  button: {
+    icon: string;
+    label: string;
+    to: {
+      name: string;
+    };
+  };
+}>();
+
+const route = useRoute();
+
+const isActive = computed(() => route.name === props.button.to.name);
+</script>
+
+<template>
+  <UButton
+    :icon="button.icon"
+    :label="button.label"
+    :to="button.to"
+    :variant="isActive ? 'subtle' : 'ghost'"
+    class="w-full"
+    size="lg"
+  />
+</template>
--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,26 +1,65 @@
 <script setup lang="ts">
-import GeneralButton from '@/components/sidebar/GeneralButton.vue';
-import BreakScheduleButton from '@/components/sidebar/BreakScheduleButton.vue';
-import BehaviorButton from '@/components/sidebar/BehaviorButton.vue';
-import NotifButton from '@/components/sidebar/NotifButton.vue';
-import BreakMessagesButton from '@/components/sidebar/BreakMessagesButton.vue';
-import AboutButton from '@/components/sidebar/AboutButton.vue';
+import NavButton from '@/components/layout/NavButton.vue';
+import { useT } from '@/composables/t.ts';
+import { computed } from 'vue';
+
+const t = useT();
+
+const mainButtons = computed(() => [
+  {
+    to: { name: 'general' },
+    label: t('general'),
+    icon: 'lucide:settings',
+  },
+  {
+    to: { name: 'break-schedule' },
+    label: t('breakSchedule'),
+    icon: 'lucide:alarm-clock',
+  },
+  {
+    to: { name: 'behavior' },
+    label: t('behavior'),
+    icon: 'lucide:brain',
+  },
+  {
+    to: { name: 'notification-alerts' },
+    label: t('notificationsAndAlerts'),
+    icon: 'lucide:bell',
+  },
+  {
+    to: { name: 'break-messages' },
+    label: t('breakMessages'),
+    icon: 'lucide:message-circle',
+  },
+]);
+
+const secondaryButtons = computed(() => [
+  {
+    to: { name: 'about' },
+    label: t('aboutSoftware'),
+    icon: 'lucide:info',
+  },
+]);
 </script>
 
 <template>
   <div class="h-full w-1/3 fixed bg-inherit p-5 flex flex-col justify-between">
     <!-- main buttons -->
     <div class="space-y-1 w-full">
-      <GeneralButton />
-      <BreakScheduleButton />
-      <BehaviorButton />
-      <NotifButton />
-      <BreakMessagesButton />
+      <NavButton
+        v-for="button in mainButtons"
+        :key="button.label"
+        :button="button"
+      />
     </div>
 
     <!-- about software -->
     <div class="w-full">
-      <AboutButton />
+      <NavButton
+        v-for="button in secondaryButtons"
+        :key="button.label"
+        :button="button"
+      />
     </div>
   </div>
 </template>
