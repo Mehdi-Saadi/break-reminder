9a677d3187a967ab0ad506594af8d06c4d1aa0e2
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-08 20:43:46 +0330

    use check and notify updates

--- src/composables/updater.ts
+++ src/composables/updater.ts
@@ -2,10 +2,14 @@ import { useUpdaterStore } from '@/stores/updater.ts';
 import { useEventListener } from '@vueuse/core';
 
 export const useUpdater = () => {
-  const { checkForUpdates } = useUpdaterStore();
+  const { checkAndNotifyUpdates } = useUpdaterStore();
 
-  const checkForUpdatesOnOnline = (): void => {
-    useEventListener('online', checkForUpdates, { once: true });
+  const checkForUpdatesOnOnline = async (): Promise<void> => {
+    if (navigator.onLine) {
+      await checkAndNotifyUpdates();
+    } else {
+      useEventListener('online', checkAndNotifyUpdates, { once: true });
+    }
   };
 
   return {
