49cbad2eff16561205ee6d67ae0f246273a5a5bf
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-14 16:55:05 +0330

    fix type error

--- src/shared/composables/t.ts
+++ src/shared/composables/t.ts
@@ -4,8 +4,10 @@ import { useI18n } from 'vue-i18n';
 export const useT = () => {
   const { t } = useI18n();
 
+  type Rest = Parameters<typeof t> extends [any, ...infer P] ? P : never;
+
   return (
     key: KeyWord,
-    ...params: any[]
-  ): string => t(key, ...params);
+    ...params: Partial<Rest>
+  ): string => t(key, ...(params as Rest));
 };
