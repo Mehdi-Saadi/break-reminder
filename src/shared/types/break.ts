116d0426364b1e170ff0edc62eb3a2ade5b97de4
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-14 11:02:48 +0330

    implement shared break types

---
+++ src/shared/types/break.ts
@@ -0,0 +1,13 @@
+import { Second } from '@/shared/types/time';
+
+export interface BreakWindowPayload {
+  message?: string;
+  timeout: Second;
+  showSkipBtn: boolean;
+  showPostponeBtn: boolean;
+}
+
+export const BREAK_WINDOW_EVENT = Object.freeze({
+  skip: 'break-window-skip',
+  postpone: 'break-window-postpone',
+});
