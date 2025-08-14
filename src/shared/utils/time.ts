2a17365fc7b6267de45368b1fcae13503c04233b
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-14 14:57:55 +0330

    remove unused util

--- src/shared/utils/time.ts
+++ src/shared/utils/time.ts
@@ -6,8 +6,6 @@ export const secondsToMilliseconds = (seconds: Second): Millisecond => seconds *
 
 export const minutesToMilliseconds = (minutes: Minute): Millisecond => secondsToMilliseconds(minutesToSeconds(minutes));
 
-export const millisecondsToSeconds = (milliseconds: Millisecond): Second => Math.floor(milliseconds / 1000) as Second;
-
 export const secondsToMinutes = (seconds: Second): Minute => Math.floor(seconds / 60) as Minute;
 
 export const numberToTwoDigits = (num: number): string => String(num).padStart(2, '0');
