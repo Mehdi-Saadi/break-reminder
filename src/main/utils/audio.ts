5d2abebd9c0e2acb9878f4fa928beca5827d77d5
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-08-14 11:22:46 +0330

    add audio utils

---
+++ src/main/utils/audio.ts
@@ -0,0 +1,11 @@
+export const playAudio = async (url: string): Promise<void> => {
+  const audioContext = new AudioContext();
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+  const source = audioContext.createBufferSource();
+  source.buffer = audioBuffer;
+  source.connect(audioContext.destination);
+  source.start();
+};
