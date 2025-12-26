6a6c04aa351af13296bf94f0a929c465398a28a6
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-12-26 20:11:24 +0330

    feat (vue-devtools): install and configure vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import ui from '@nuxt/ui/vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST
@@ -19,6 +20,7 @@ export default defineConfig(async () => ({
       },
       colorMode: false,
     }),
+    vueDevTools({ }),
   ],
   resolve: {
     alias: {
