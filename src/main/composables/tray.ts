0647e5e56b6cfc69cffe441f183311eaeb893075
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2026-01-03 16:47:46 +0330

    feat(tray): show tray menu on left click

--- src/main/composables/tray.ts
+++ src/main/composables/tray.ts
@@ -42,7 +42,6 @@ export function useTray() {
       icon: await defaultWindowIcon() || undefined,
       menu: await createMenu(),
       tooltip: 'Break Reminder',
-      showMenuOnLeftClick: false,
     })
   }
 
